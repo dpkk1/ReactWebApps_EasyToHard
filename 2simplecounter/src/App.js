4323ee668cc5d7e7a85709db44899f7a1e52ba20
dpkk1 <dpkkmr1600@gmail.com>
2022-08-15 23:24:12 +0530

    SimpleCounter ReactApp Done

---
+++ 2simplecounter/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="App moveCenter">
+        <button onClick={this.increment} className="counter">
+          +
+        </button>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.decrement} className="counter">
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
